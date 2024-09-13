5f115e89d9ebfe06625c982296c274d22cf38729
Avila George <george.avila@external.bper.it>
2024-09-13 14:46:41 +0200

    digitalocean

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
     endpoint: 'https://cimapiazzi-be.onrender.com/',
     googleMapsApiKey: 'AIzaSyA1JYdBfuWQxh1CIiQLpiai0A-EPxYtogI',
     spacesEndpoint: 'fra1.digitaloceanspaces.com',
-    accessKeyId: 'DO00R74W2WNVTG49WKFUS',
-    secretAccessKey: 'P3FOkWhN7rm9OFBd3nuPoRpY+HEspRFINBfkqvfkeO0S',
+    accessKeyId: 'DO003RNZ7LBYBUFYZN3Z',
+    secretAccessKey: 'G9lWYNPj24SgTQjxvAf51TkAItFdUuRrVdtYDHT4vm0',
     bucketName: 'cimapiazzi.images',
     adminEmails: [
         'avila@cimapiazzi.com',
